7b17c87d96ac545235c930a5983cf57c08f97065
Anton Berezhnoj <berejant@gmail.com>
2015-09-20 22:38:31 +0300

    Ignore config database.php

--- api/config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-    'database_type' => 'mysql',
-    'database_name' => 'mbti',
-    'server' => 'localhost',
-    'username' => 'mbti',
-    'password' => '',
-    'charset' => 'utf8'
-);
